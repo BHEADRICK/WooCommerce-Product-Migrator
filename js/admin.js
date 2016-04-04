443834fe39d22d505b2ce6f68fccdce2c37b3f51
Bryan Headrick <developer@pi-inc.com>
2016-04-04 18:08:02 -0400

    Added wc rest api wrapper added fields to save key and secret and url for remote site added function to get all products from remote site

--- js/admin.js
+++ js/admin.js
@@ -2,7 +2,15 @@
 
 //Surround this with jQuery(document).ready() if this isn't in the footer
 (function($) {
-	
+    var data = {
+
+    };
+	$.post(ajaxurl, data, function(resp){
+        if(typeof resp == 'string'){
+            resp = JSON.parse(resp);
+        }
+        console.log(resp);
+    })
 	// $ Works! You can test it with next line if you like
 	// console.log($);
 	
--- woocommerce-product-migrator.php
+++ woocommerce-product-migrator.php
@@ -16,7 +16,8 @@ Author URI: https://authorurl.com
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
+require_once 'boots/api.php';
+require __DIR__ . '/vendor/autoload.php';
 class WooCommerceProductMigrator {
 
 	/*--------------------------------------------*
@@ -24,14 +25,26 @@ class WooCommerceProductMigrator {
 	 *--------------------------------------------*/
 	const name = 'WooCommerce Product Migrator';
 	const slug = 'woocommerce-product-migrator';
+	const version = '1.0';
+	private $prefix;
+	private $Settings;
 
 	/**
 	 * Constructor
 	 */
 	function __construct() {
 		//register an activation hook for the plugin
+		$this->prefix = str_replace('-', '_', self::slug);
 		register_activation_hook( __FILE__, array( $this, 'install_woocommerce_product_migrator' ) );
+		$args = array(
+		'ABSPATH' => __FILE__,
+    'APP_ID' => $this->prefix,
+    'APP_NICK' => self::name,
+    'APP_VERSION' => self::version,
+    'APP_MODE' => 'dev');
 
+		$this->Settings = new WCPMOptions($args);
+		add_action('admin_menu', array($this->Settings, 'admin'));
 		//Hook up to the init action
 		add_action( 'init', array( $this, 'init_woocommerce_product_migrator' ) );
 	}
@@ -79,6 +92,25 @@ class WooCommerceProductMigrator {
 		// TODO define your filter method here
 	}
 
+	function get_products(){
+		$key = get_option($this->prefix.'_wc_key');
+		$secret = get_option($this->prefix.'_wc_secret');
+		$site_url = get_option($this->prefix.'_wc_site_url');
+		$options = array(
+			'debug'=>true,
+			'ssl_verify'=>false,
+			'validate_url'=>true
+		);
+		if($key && $secret && $site_url) {
+
+			$client = new WC_API_Client($site_url, $key, $secret, $options);
+
+			$products = $client->products->get();
+
+			echo json_enconde($products);
+		}
+		wp_die();
+	}
 	function render_shortcode($atts) {
 		// Extract the attributes
 		extract(shortcode_atts(array(
@@ -127,4 +159,61 @@ class WooCommerceProductMigrator {
 	} // end load_file
 
 } // end class
+
+
+if(!class_exists('WCPMOptions')) :
+
+class WCPMOptions
+{
+	private $Boots;
+	private $Settings;
+	private $prefix;
+
+	public function __construct($Args)
+	{
+		$this->Boots = new Boots('plugin', $Args);
+		$this->Settings = $Args;
+		$this->prefix = $Args['APP_ID'];
+		$this->Boots->Admin;
+	}
+
+	public function admin()
+	{
+
+
+		$this->Boots->Admin
+			->menu('mop_admin')
+			->add('textbox', array(
+				'title' => 'WC API Key',
+				'name'  => $this->prefix.'_wc_key',
+				'help'  => 'API Key'
+			))->add('textbox', array(
+				'title' => 'WC API Secret',
+				'name'  => $this->prefix.'_wc_secret',
+				'help'  => 'API SECRET'
+			))->add('textbox', array(
+				'title' => 'WC Remote site url',
+				'name'  => $this->prefix.'_wc_site_url',
+				'help'  => 'http://my-example-site.com'
+			))
+			->add('_', array($this, 'my_custom_html'))
+
+			->done();
+	}
+
+	public function my_custom_html(){
+		echo 'hello world';
+		$key = get_option($this->prefix.'_wc_key');
+		$secret = get_option($this->prefix.'_wc_secret');
+		$site_url = get_option($this->prefix.'_wc_site_url');
+		if($key && $secret && $site_url){
+
+		echo 'hello world';
+		}
+
+	}
+}
+endif;
+
+
 new WooCommerceProductMigrator();
